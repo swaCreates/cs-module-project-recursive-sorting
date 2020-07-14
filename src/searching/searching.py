fc4fc621eeeb59221eb5bcd9e3add22f96c64a9f
Francois Chery <swa_isthecreator@Francoiss-MBP.fios-router.home>
2020-07-14 18:33:41 -0400

    completed binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -2,6 +2,18 @@
 def binary_search(arr, target, start, end):
     # Your code here
 
+    mid = (start + end) // 2
+    
+    while start <= end:
+        if arr[mid] == target:
+            return mid
+        elif target < arr[mid]:
+            return binary_search(arr, target, start, mid - 1)
+        elif target > arr[mid]:
+            return binary_search(arr, target, mid + 1, end)
+    return -1
+
+
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -10,5 +22,6 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # Your code here
+    pass
+#     # Your code here
 
